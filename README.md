201ada09c24746ad3d8467eb01f5aeb7f05da62d
handred800 <handred800@gmail.com>
2021-09-09 18:24:03 +0800

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
-# firetest
+# meme-db
+firebase image db testing
 
-## Project setup
-```
-npm install
-```
+## tech
+- firebase v9  
+- vue 2
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,139 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- auth section -->
+    <form @submit.prevent="signIn">
+      <fieldset v-if="this.uid === ''">
+        <input type="text" v-model="email" placeholder="email">
+        <input type="password" v-model="password" placeholder="password">
+        <button type="submit">sign in</button>        
+      </fieldset>
+      <fieldset v-else>
+        <button type="button" @click="signOut">sign out</button>
+      </fieldset>
+    </form>
+    <!-- upload section -->
+    <form @submit.prevent="uploadMeme" v-if="this.uid !== ''">
+      <input type="file" accept="image/*" @change="changeData">
+      <button type="submit">upload</button>
+    </form>
+    <!-- images section -->
+    <ul v-if="memeList.length > 0">
+      <li v-for="meme in memeList" :key="meme.id">
+        <img class="thumbnail" :src="meme.path" alt="">
+        <input type="text" v-model="meme.path" readonly @click="copyvalue">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getFirestore, collection, addDoc, onSnapshot } from "firebase/firestore"; 
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const firebaseApp = initializeApp({
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: "lizz-meme.firebaseapp.com",
+  projectId: "lizz-meme",
+  storageBucket: "lizz-meme.appspot.com",
+  messagingSenderId: process.env.VUE_APP_SENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_GTAG
+});
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp)
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return{
+      email: '',
+      password: '',
+      uid: '',
+      memeList: [],
+      fileData: {
+        file: null,
+        filename: '',
+      },
+      memeData: {
+        tags: [''],
+        path: '',
+      }
+    }
+  },
+  methods: {
+    signIn() {
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then(() => {
+          // const user = userCredential.user;
+          // console.log(user);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+    signOut() {
+      signOut(auth).then(() => {
+      }).catch((error) => {
+        console.log(error.message);
+      });
+    },
+    changeData(e) {
+      this.fileData.file = e.target.files[0];
+      this.fileData.filename = this.fileData.file.name;
+    },
+    uploadMeme() {
+      this.uploadFile()
+            .then((snapshot) => {
+              // console.log(snapshot)
+              return this.getImageUrl(snapshot.metadata.fullPath);
+            })
+            .catch((error) => {
+              console.log(error.message);
+            })
+            .then((url) => {
+              this.memeData.path = url;
+              return this.createDoc();
+            })
+    },
+    uploadFile() {
+      return uploadBytes(ref(storage, `images/${this.fileData.filename}`), this.fileData.file)
+    },
+    createDoc() {
+      return addDoc(collection(db, "memes"), this.memeData);
+    },
+    getImageUrl(path) {
+      return getDownloadURL(ref(storage, path))
+    },
+    copyvalue(e) {
+      e.target.select();
+      document.execCommand('copy');
+    },
+  },
+  mounted() {
+    onAuthStateChanged(auth, user => {
+      // console.log('auth has changed')
+      this.uid = user ? user.uid : '';
+    });
+
+    onSnapshot(collection(db, 'memes'), (Snapshots) => {
+        this.memeList = [];
+        Snapshots.forEach((doc) => {
+          let meme = Object.assign(doc.data(), { id: doc.id })
+          this.memeList.push(meme);
+        })
+      })
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .thumbnail{
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+  }
 </style>
